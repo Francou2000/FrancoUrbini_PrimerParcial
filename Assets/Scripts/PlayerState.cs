53955b9299bb6d50f7b5d46dd50ff7c04d97ae41
Franco Urbini <francourbini@gmail.com>
2024-03-24 20:31:10 -0300

    start hp bar

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public static PlayerState Instance { get; private set; }
+
+    public float currentHealth;
+    public float maxHealth = 100;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        
+    }
+}
