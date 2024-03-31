82196b542bea0136827eade172495edffbfbf06d
Franco Urbini <francourbini@gmail.com>
2024-03-30 23:27:58 -0300

    boss follow up

--- Assets/Scripts/Enemies/Boss/AIBoss.cs
+++ Assets/Scripts/Enemies/Boss/AIBoss.cs
@@ -4,15 +4,89 @@ using UnityEngine;
 
 public class AIBoss : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator animator;
+
+    public float moveSpeed = 1f;
+    public float health = 1f;
+
+    public Transform playerCheck;
+    public float playerDistance = 3f;
+    public LayerMask playerMask;
+
+    public bool isRunning;
+    public bool isAttackRange;
+
+    public Transform player;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        DetectPlayer();
+
+        if (isAttackRange == true)
+        {
+            StartCoroutine(AttackRoutine());
+        }
+
+        if (health <= 0f)
+        {
+            StartCoroutine(DeathRoutine());
+        }
+    }
+
+    public void DetectPlayer()
+    {
+        isRunning = Physics.CheckSphere(playerCheck.position, playerDistance, playerMask);
+        isAttackRange = Physics.CheckSphere(playerCheck.position, playerDistance / 2, playerMask);
+
+        if (isRunning)
+        {
+            animator.SetBool("isRunning", true);
+
+            moveSpeed = 1;
+
+            transform.LookAt(player.position);
+            transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+
+            moveSpeed = 0;
+        }      
+    }
+
+    private IEnumerator DeathRoutine()
+    {
+        animator.SetTrigger("Dead");
+
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+        Destroy(this.gameObject);
+    }
+
+    private IEnumerator AttackRoutine()
+    {
+        animator.SetTrigger("Attack1");
+
+        gameObject.GetComponent<BoxCollider>().enabled = true;
+
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+        animator.SetTrigger("Attack2");
+
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, playerDistance);
     }
 }
